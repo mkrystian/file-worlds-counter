ba849135d6f95f976d5a11c0b36e3ae9463e986c
Krystian <majewski.kristian@gmail.com>
2019-03-11 20:32:56 +0100

    init commit

---
+++ src/test/groovy/pl/krystianmajewski/fileworldscounter/model/WorldsContainerTest.groovy
@@ -0,0 +1,17 @@
+package pl.krystianmajewski.fileworldscounter.model
+
+import spock.lang.Specification
+
+/**
+ * @author Krystian Majewski
+ * @since 11.03.2019.
+ */
+class WorldsContainerTest extends Specification {
+
+    def "Should run test"() {
+        given:
+            def a = "a"
+        expect:
+            a == "a"
+    }
+}
